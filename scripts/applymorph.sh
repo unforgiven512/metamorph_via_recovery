40338cf8bbf293bdd46dbdc761fc41fda890ea01
Gerad Munsch <unforgiven512@buprenorphine.piratenet>
2011-05-27 17:56:41 -0400

    finished shit up, ready to test

--- scripts/applymorph.sh
+++ scripts/applymorph.sh
@@ -1,3 +1,37 @@
 #!/sbin/sh
 
-echo "`date -u +%H:%M:%S` -- Applying theme!" >> /tmp/metamorph.log
+# temporary theme directory: /tmp/metamorph/theme/out
+BASE_DIR="/tmp/metamorph/theme/out"
+MMREC_LOG=/sdcard/mmrec/mmrec.log
+
+echo "`date -u +%H:%M:%S` -- Applying theme!" >> $MMREC_LOG
+
+for subtheme in /tmp/metamorph/theme/out/*/
+do
+	if [[ "$subtheme" == "framework-res" ]]
+	then
+		cd "$subtheme"
+		zip -r /system/framework/framework-res.apk ./*
+		zipalign -f 4 /system/framework/framework-res.apk /tmp/framework-res.apk
+		mv /tmp/framework-res.apk /system/framework/framework-res.apk
+		echo "`date -u +%H:%M:%S` -- Themed framework-res.apk" >> $MMREC_LOG
+	else
+		cd "$subtheme"
+		if [[ -e /system/app/"$subtheme".zip ]]
+		then
+			zip -r /system/app/"$subtheme".apk ./*
+			zipalign -f 4 /system/app/"$subtheme".apk /tmp/"$subtheme".apk
+			mv /tmp/"$subtheme".apk /system/app/"$subtheme".apk
+			echo "`date -u +%H:%M:%S` -- Themed $subtheme.apk" >> $MMREC_LOG
+		else
+			echo "`date -u +%H:%M:%S` -- $subtheme.apk does not exist." >> $MMREC_LOG
+		fi
+	fi
+done
+
+# clean up our mess
+sync
+sleep 2
+rm -rf /tmp/*
+sleep 1
+sync
--- scripts/startupcheck.sh
+++ scripts/startupcheck.sh
@@ -1,26 +1,28 @@
 #!/sbin/sh
 
-touch /tmp/metamorph.log
-echo "--------------------------------------------------------------------------------" >> /tmp/metamorph.log
-echo "METAMORPH VIA RECOVERY" >> /tmp/metamorph.log
-echo "--------------------------------------------------------------------------------" >> /tmp/metamorph.log
-echo "`date -u +%H:%M:%S` -- Startup Checks initialized" >> /tmp/metamorph.log
-themezip=`ls -1 /tmp/metamorph/theme/*.zip | grep .zip`
-themezip=${themezip##*/}
-echo "`date -u +%H:%M:%S` -- Theme = ${themezip%.zip}" >> /tmp/metamorph.log
-mkdir /tmp/metamorph/theme/out
+MMREC_LOG=/sdcard/mmrec/mmrec.log
+
+touch $MMREC_LOG
+echo "--------------------------------------------------------------------------------" >> $MMREC_LOG
+echo "METAMORPH VIA RECOVERY" >> $MMREC_LOG
+echo "--------------------------------------------------------------------------------" >> $MMREC_LOG
+echo "`date -u +%H:%M:%S` -- Startup Checks initialized" >> $MMREC_LOG
+themezip="`ls -1 /tmp/metamorph/theme/*.zip | grep .zip`"
+themezip="${themezip##*/}"
+echo "`date -u +%H:%M:%S` -- Theme = ${themezip%.zip}" >> $MMREC_LOG
 
 if ([ -f "/tmp/metamorph/tools/zip" ] && [ -f "/tmp/metamorph/tools/zipalign" ] && [ -f "/tmp/metamorph/theme/${themezip}" ])
 	then
 		# Awesome, everything is swell
 		# Let's go ahead and unzip our theme!
+		mkdir -p /tmp/metamorph/theme/out
 		unzip -q /tmp/metamorph/theme/${themezip} -d /tmp/metamorph/theme/out/
-		echo "`date -u +%H:%M:%S` -- Everything should be working correctly." >> /tmp/metamorph.log
+		echo "`date -u +%H:%M:%S` -- Extracted theme to /tmp/metamorph/theme/out/*." >> $MMREC_LOG
 		return 0
 	else
 		# Something is missing, exit with error
-		echo "`date -u +%H:%M:%S` -- Something went wrong, exiting." >> /tmp/metamorph.log
-		echo "`date -u +%H:%M:%S` -- Check that your theme is in the theme directory." >> /tmp/metamorph.log
-		echo "`date -u +%H:%M:%S` -- DEBUG: \$themezip = ${themezip}" >> /tmp/metamorph.log
+		echo "`date -u +%H:%M:%S` -- Something went wrong, exiting." >> $MMREC_LOG
+		echo "`date -u +%H:%M:%S` -- Check that your theme is in the theme directory." >> $MMREC_LOG
+		echo "`date -u +%H:%M:%S` -- DEBUG: \$themezip = ${themezip}" >> $MMREC_LOG
 		return 1
 fi
